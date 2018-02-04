502deaf348a6dfa80d6b1bde5e6f3dc1e62e170c
Alexandra Moore <alexmoore94@hotmail.co.uk>
2018-02-04 19:27:24 +0000

    homepage styled a bit more

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,9 +7,12 @@ import '../styles/pages/Home.css';
 class Home extends Component {
   render() {
     return (
-      <Typist>
-        Hey, I&lsquo;m Alexandra.
-      </Typist>
+      <div className="home">
+        <Typist>
+          Hey, I&lsquo;m Alexandra.
+        </Typist>
+        <img src={require('../styles/assets/palm-leaf.svg')} alt="palm leaf"/>
+      </div>
     );
   };
 };
