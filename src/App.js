8a502752395a4ff0ae7c87445d6603b2c18e3776
Alexandra Moore <alexmoore94@hotmail.co.uk>
2018-02-04 16:12:05 +0000

    added sass and moved files into styles folder

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 
-import './App.css';
+import './styles/App.css';
 
 class App extends Component {
   render() {
