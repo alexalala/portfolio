d42370bb5b72c95478e394830c494c67999f5f4e
Alexandra Moore <alexmoore94@hotmail.co.uk>
2018-02-04 15:20:34 +0000

    added routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
+
 import Navbar from './components/Navbar';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <p>Hello</p>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-const Navbar = () => {
-  return (
-    <ul>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-    </ul>
-  );
+class Navbar extends Component {
+  render() {
+    return (
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    );
+  };
 };
 
 export default Navbar;
