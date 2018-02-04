66d9e17a0b07116eb26440052421c27b8a49d79b
Alexandra Moore <alexmoore94@hotmail.co.uk>
2018-02-04 18:11:06 +0000

    react typist added to text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import Typist from 'react-typist';
+
+import 'react-typist/dist/Typist.css';
 
 class Home extends Component {
   render() {
     return (
-      <p>Home</p>
+      <Typist>
+        Hey, I&lsquo;m Alexandra.
+      </Typist>
     );
   };
 };
