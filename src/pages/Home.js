c183d95ade2150d47dc7248cdaabe619e2411ac8
Alexandra Moore <alexmoore94@hotmail.co.uk>
2018-02-04 18:41:10 +0000

    basic styles added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Typist from 'react-typist';
 
 import 'react-typist/dist/Typist.css';
+import '../styles/pages/Home.css';
 
 class Home extends Component {
   render() {
