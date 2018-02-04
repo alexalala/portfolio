d57e8f9c99d3118b8c6d32ed246a5ca970d5a5e8
Alexandra Moore <alexmoore94@hotmail.co.uk>
2018-02-04 21:24:55 +0000

    contact form sorted

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 
+import '../styles/pages/Contact.css';
+
 class Contact extends Component {
   render() {
     return (
-      <p>Contact</p>
+      <div className="contact">
+        <p>Feel free to get in touch!</p>
+        <form action="https://formspree.io/contact@alexandramoo.re" method="POST">
+          <label>
+            Your name:
+            <input type="text" name="name"></input>
+          </label>
+          <label>
+            Your email:
+            <input type="email" name="_replyto"></input>
+          </label>
+          <label>
+            Description:
+            <textarea rows="3" cols="3" name="description"></textarea>
+          </label>
+          <input type="submit" value="Send"></input>
+        </form>
+      </div>
     );
   };
 };
