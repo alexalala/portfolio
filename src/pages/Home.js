ea88c2af3c92a411efe54bb9496dba7a48acab5b
Alexandra Moore <alexmoore94@hotmail.co.uk>
2018-05-07 20:48:15 +0100

    About page rewritten with styled-components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import styled from "styled-components";
+import styled from 'styled-components';
 import Typist from 'react-typist';
 
 import 'react-typist/dist/Typist.css';
