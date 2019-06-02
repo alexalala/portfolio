4d6dd2f7525e6d3a2fe3b687de2256a5798e829b
Alexandra Moore <alexmoore94@hotmail.co.uk>
2019-06-02 21:55:49 +0100

    hover on contact  links

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
-import { primary_colour } from '../assets/styles/variables.js';
+import { primary_colour, black } from '../assets/styles/variables.js';
 import Subheader from '../components/Subheader.js';
 import PageWrapper from '../layouts/PageWrapper.jsx';
 
@@ -10,8 +10,8 @@ class Contact extends Component {
     return (
       <PageWrapper className={this.props.className}>
         <Subheader>Feel free to get in touch!</Subheader>
-        <a href="mailto:mail@alexandramoo.re">Email me at mail@alexandramoo.re</a>
-        <a href="https://twitter.com/alexandraamoore">Tweet me @alexandraamoore</a>
+        <a href="mailto:mail@alexandramoo.re">Email me - mail@alexandramoo.re</a>
+        <a href="https://twitter.com/alexandraamoore">Tweet me - @alexandraamoore</a>
       </PageWrapper>
     );
   };
@@ -21,6 +21,21 @@ const styledContact = styled(Contact)`
   background-color: ${primary_colour};
   margin: 0;
   width 100%;
+
+  a {
+    background-image: linear-gradient(180deg,transparent 65%,#a9d8f3 0);
+    background-size: 0 100%;
+    background-repeat: no-repeat;
+    text-decoration: none;
+    color: ${black};
+    font-size: 3rem;
+
+    &:hover {
+      background-size: 100% 100%;
+      cursor: pointer;
+      color: ${black};
+    }
+  }
 `;
 
 export default styledContact;
